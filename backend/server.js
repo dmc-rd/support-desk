85f2c62ff0a928edb7ed0e9c6a9ed4c9a5ac3d24
David McCullough <davidmccullough@Davids-MacBook-Pro.local>
2023-11-07 20:39:30 -0700

    Backend scafold done

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-console.log('Server....')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const colors = require('colors');
+const connectDB = require('./config/db');
+const { errorHandler } = require('./middleware/errorMiddleware');
+const PORT = process.env.PORT || 5001;
+
+//Connect to DB
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the support API' });
+});
+
+//Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
